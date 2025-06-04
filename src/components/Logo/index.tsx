877418175f7c61fd815be798dfac7d6c987a2c07
Victor Furustubbe <mantykanto2@gmail.com>
2025-06-04 11:50:22 +0200

    added naming converntions changes and disable lazing loading on logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 const Logo = () => {
     return (
         <Link href='/'>
-            <Image src='/pirate-logo.png' alt="Logo" width={100} height={100} style={{width: '100px' , height: 'auto'}}/>
+            <Image priority={true} src='/pirate-logo.png' alt="Logo" width={100} height={100} style={{width: '100px' , height: 'auto'}}/>
         </Link>
     )
 }
