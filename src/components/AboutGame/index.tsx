92823c54e8a48ccb48d237cdfb093e16a131e89c
VaishaliGovindaraj <vaishali.govindaraj28@gmail.com>
2025-06-04 22:43:33 +0200

    updating Sidebar Logic

---
+++ src/components/AboutGame/index.tsx
@@ -0,0 +1,10 @@
+const AboutGame = () => {
+    return(
+        <div id="about_game" className="min-h-screen bg-green-200">
+            About Game
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis vero accusantium aut vitae voluptatem inventore ipsam sint dolor quod nam.
+        </div>
+    )
+}
+
+export default AboutGame;
---
+++ src/components/Features/index.tsx
@@ -0,0 +1,10 @@
+const Features = () => {
+    return(
+        <div id="features" className="min-h-screen bg-amber-200">
+            Features
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis vero accusantium aut vitae voluptatem inventore ipsam sint dolor quod nam.
+        </div>
+    )
+}
+
+export default Features;
