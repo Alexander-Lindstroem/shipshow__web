0d9b81afd4d11852016a0fe2987cb10f446f4fdc
Victor Furustubbe <mantykanto2@gmail.com>
2025-06-05 11:56:03 +0200

    added changes to mobile header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import Logo from "../Logo"
 const Header = () => {
     return (
         <div className="w-screen border-black-100 border bg-black opacity-70 md:w">
-            <header className=" mx-4 my-4 flex flex-col items-center h-auto gap-4 md:mx-20 md:flex-row md:justify-between md:h-24">
+            <header className=" mx-4 my-4 flex justify-between items-center h-auto gap-4 md:mx-20 md:flex-row  md:h-24">
                 <Logo/>
                 <Navigation/>
             </header>
