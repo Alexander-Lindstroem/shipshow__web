dc9ac83b761c573ac1fe4613a8b88869bbead643
Victor Furustubbe <mantykanto2@gmail.com>
2025-06-10 17:41:55 +0200

    added header fixes

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -5,7 +5,7 @@ const Navigation = () => {
     return (
         <Link href='/crew' className="flex items-center gap-1">
             <Image src='/pirate-ship-icon.png' alt="pirate-ship-icon" width={100} height={100} style={{width: '25px' , height: 'auto'}}/>
-            <h3 className='text-white'>The crew</h3>
+            <h3 className='text-white font-asheader text-2xl'>The crew</h3>
         </Link>
     )
 }
