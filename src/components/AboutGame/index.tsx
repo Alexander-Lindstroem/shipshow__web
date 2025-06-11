ecde5a222f46e3f6dfdb54c6d110ba22fa9de901
kavitha0691 <kavitha0691@gmail.com>
2025-06-11 19:47:11 +0200

    uses main branch global css tailwind variable

--- src/components/AboutGame/index.tsx
+++ src/components/AboutGame/index.tsx
@@ -18,13 +18,13 @@ export default function AboutGame() {
           />
         </div>
         <div className="w-full lg:w-1/3 md:w-[80%] text-inherit">
-          <h2 className="text-3xl lg:text-3xl md:text-4xl sm:text-2xl font-medium leading-3 tracking-normal font-name mb-6 lg:mb-8 md:mb-8 sm:mb-4">
+          <h2 className="text-3xl lg:text-3xl md:text-4xl sm:text-2xl font-medium leading-3 tracking-normal font-asheader mb-6 lg:mb-8 md:mb-8 sm:mb-4">
             What is Absolute Shipshow?
           </h2>
-          <p className="text-xl lg:text-lg md:text-xl sm:text-base font-info mb-3 leading-6">
+          <p className="text-xl lg:text-lg md:text-xl sm:text-base font-asbody mb-3 leading-6">
             Absolute Shipshow is a <span className="font-semibold text-black">chaotic, co-op</span> combat game where you and your friends play as ill-prepared pirates fighting off waves of enemies aboard ever-sinking ships.
           </p>
-          <p className="text-xl lg:text-lg md:text-xl sm:text-base font-info leading-6">
+          <p className="text-xl lg:text-lg md:text-xl sm:text-base font-asbody leading-6">
             Work together, improvise with whatever you find onboard, and survive long enough to loot the treasure — before the whole ship goes under!
           </p>
         </div>
@@ -42,13 +42,13 @@ export default function AboutGame() {
           />
         </div>
         <div className="w-full lg:w-1/3 md:w-[80%] text-inherit">
-          <h2 className="text-3xl lg:text-3xl md:text-4xl sm:text-2xl font-medium leading-3 tracking-normal font-name mb-6 lg:mb-8 md:mb-8 sm:mb-4">
+          <h2 className="text-3xl lg:text-3xl md:text-4xl sm:text-2xl font-medium leading-3 tracking-normal font-asheader mb-6 lg:mb-8 md:mb-8 sm:mb-4">
             How to play?
           </h2>
-          <p className="text-xl lg:text-lg md:text-xl sm:text-base font-info mb-3 leading-6">
+          <p className="text-xl lg:text-lg md:text-xl sm:text-base font-asbody mb-3 leading-6">
             Defeat the enemies, collect your treasure to your treasure chest and fight for your booty!
           </p>
-          <p className="text-xl lg:text-lg md:text-xl sm:text-base font-info leading-6">
+          <p className="text-xl lg:text-lg md:text-xl sm:text-base font-asbody leading-6">
             In order to win you need to defeat the last waves of enemies before they can steal all your treasure or all players are knocked unconscious.
           </p>
         </div>
