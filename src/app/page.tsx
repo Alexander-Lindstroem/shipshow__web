c48a76c920b8d78d2db04431c24b6a01d85bd182
VaishaliGovindaraj <vaishali.govindaraj28@gmail.com>
2025-06-12 23:43:57 +0200

    update review comment changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
 'use client'
 import { useEffect, useState } from "react";
 import Sidebar from "@/components/sidebar";
-import MainPage from "@/components/MainPage";
-import AboutGame from "@/components/AboutGame";
-import Features from "@/components/Features";
-import Characters from "@/components/Characters";
-
-const sectionIds:string[] = ["main_page","about_game","features","characters"];
+import { chest_names } from "@/utils/data";
 
 const Home = () => {
   const [chosenSection, setChosenSection] = useState<string | null>(null);
@@ -14,12 +9,10 @@ const Home = () => {
   useEffect(() => {
     const handleScroll = () => {
       let currentSection: string | null = null;
-
-      for (const id of sectionIds) {
-        const el = document.getElementById(id);
-        if (el) {
-          const rect = el.getBoundingClientRect();
-          // Check if section top is in view
+      for (const id of chest_names) {
+        const page_id = document.getElementById(id);
+        if (page_id) {
+          const rect = page_id.getBoundingClientRect();
           if (rect.top >= 0 && rect.top < window.innerHeight / 2) {
             currentSection = id;
             break;
@@ -41,10 +34,6 @@ const Home = () => {
   return (
     <div>
       <Sidebar sectionChosed={chosenSection} setSection={setChosenSection} />
-      <MainPage />
-      <AboutGame />
-      <Features />
-      <Characters />
     </div>
   );
 };
--- src/components/MainPage/index.tsx
+++
@@ -1,9 +0,0 @@
-const MainPage = () => {
-    return(
-        <div id="main_page" className="min-h-screen bg-blue-200">
-        Main Page
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis vero accusantium aut vitae voluptatem inventore ipsam sint dolor quod nam.
-        </div>
-    )
-}
-export default MainPage;
