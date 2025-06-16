4a051c0ecac621586075c5f4d459aaaa4396fabc
kavitha0691 <kavitha0691@gmail.com>
2025-06-16 14:29:29 +0200

    changes font and image width

--- src/components/AboutGame/index.tsx
+++ src/components/AboutGame/index.tsx
@@ -4,51 +4,51 @@ import Image from "next/image";
 
 export default function AboutGame() {
   return (
-    <section id="about_game" className="w-full bg-orange-100 pt-28 sm:pt-20 md:pt-24 lg:pt-28 pb-12 px-4 sm:px-8 md:px-18 lg:px-24 overflow-hidden pl-24 sm:pl-8 md:pl-20 lg:pl-28">
-      <div className="flex flex-col lg:flex-row items-center justify-center gap-8 lg:gap-20 md:gap-15 sm:gap-10 mb-8">
-        <div className="w-full md:w-[80%] sm:w-[90%] lg:w-1/3">
+    <section id="about_game" className="w-full min-h-screen bg-orange-100  pb-12 px-4 sm:px-8 md:px-18 lg:px-24 overflow-hidden pl-20  md:pl-20 lg:pl-28 pt-16">
+      <div className="mx-auto flex flex-col lg:flex-row items-center justify-center gap-8 lg:gap-20 md:gap-15 sm:gap-10 mb-8">
+        <div className="w-full lg:w-[400px] md:w-[500px]">
           <Image
             src="/ship.png"
             alt="What is Absolute Shipshow?"
             width={500}
-            height={300}
-            className="w-full h-[320px] lg:h-[250px] md:h-[350px] sm:h-[300px] shadow-md"
+            height={400}
+            className="w-full md:w-[400px] h-[320px] lg:h-[300px] md:h-[400px] sm:h-[300px] shadow-md"
             priority
           />
         </div>
         
-        <div className="w-full lg:w-1/3 md:w-[80%] text-inherit">
-          <h2 className="text-3xl lg:text-3xl md:text-4xl sm:text-2xl font-medium leading-3 tracking-normal font-asheader mb-6 lg:mb-8 md:mb-8 sm:mb-4">
+        <div className="w-full lg:w-[400px] md:w-[80%] text-inherit text-justify">
+          <h2 className="text-xl lg:text-3xl md:text-3xl font-medium leading-3 tracking-normal font-asheader mb-6 lg:mb-8 md:mb-8 ">
             What is Absolute Shipshow?
           </h2>
-          <p className="text-xl lg:text-lg md:text-xl sm:text-base font-asbody mb-3 leading-6">
+          <p className="text-l lg:text-lg md:text-l font-asbody mb-3 leading-6">
             Absolute Shipshow is a <span className="font-semibold text-black">chaotic, co-op</span> combat game where you and your friends play as ill-prepared pirates fighting off waves of enemies aboard ever-sinking ships.
           </p>
-          <p className="text-xl lg:text-lg md:text-xl sm:text-base font-asbody leading-6">
+          <p className="text-l lg:text-lg md:text-l font-asbody leading-6">
             Work together, improvise with whatever you find onboard, and survive long enough to loot the treasure — before the whole ship goes under!
           </p>
         </div>
       </div>
 
       <div className="flex flex-col  items-center justify-center gap-8 lg:gap-20 md:gap-15 sm:gap-10 mb-8 lg:flex-row-reverse">
-        <div className="w-full md:w-[80%] sm:w-[90%] lg:w-1/3">
+        <div className="lg:w-[400px] md:w-[500px]">
           <Image
             src="/fight_ship.png"
             alt="How to play?"
             width={500}
-            height={300}
-            className="w-full h-[320px] lg:h-[250px] md:h-[350px] sm:h-[300px] shadow-md"
+            height={400}
+            className="w-full h-[350px]  lg:h-[300px] md:h-[400px]"
             priority
           />
         </div>
-        <div className="w-full lg:w-1/3 md:w-[80%] text-inherit">
-          <h2 className="text-3xl lg:text-3xl md:text-4xl sm:text-2xl font-medium leading-3 tracking-normal font-asheader mb-6 lg:mb-8 md:mb-8 sm:mb-4">
+        <div className="w-full lg:w-[400px] md:w-[80%] text-inherit text-justify">
+          <h2 className="text-xl lg:text-3xl md:text-3xl  font-medium leading-3 tracking-normal font-asheader mb-6 lg:mb-8 md:mb-8 sm:mb-4">
             How to play?
           </h2>
-          <p className="text-xl lg:text-lg md:text-xl sm:text-base font-asbody mb-3 leading-6">
+          <p className="text-l lg:text-lg md:text-l sm:text-base font-asbody mb-3 leading-6">
             Defeat the enemies, collect your treasure to your treasure chest and fight for your booty!
           </p>
-          <p className="text-xl lg:text-lg md:text-xl sm:text-base font-asbody leading-6">
+          <p className="text-l lg:text-lg md:text-l sm:text-base font-asbody leading-6">
             In order to win you need to defeat the last waves of enemies before they can steal all your treasure or all players are knocked unconscious.
           </p>
         </div>
