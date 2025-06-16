1aafbf8f8e043c027e6f9dac9f1209c1b8063ddc
Victor Furustubbe <mantykanto2@gmail.com>
2025-06-16 12:27:40 +0200

    added changes in pirate-plank image

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="relative min-h-28 h-28 w-full overflow-hidden">
-      <div className="absolute top-0 left-1/2 w-[120vw] h-32 -translate-x-1/2">
+    <footer className="relative h-28 w-full overflow-hidden">
+      <div className="absolute top-0 left-1/2 h-28 w-[120vw] -translate-x-1/2">
         <Image
-          src="/plank.png"
+          src="/pirate-plank.png"
           alt="Footer Plank"
           fill
           priority
-          className="object-cover"
+          className="object-fill"
         />
       </div>
       <div className="relative z-10 flex h-full items-center justify-center text-white">
