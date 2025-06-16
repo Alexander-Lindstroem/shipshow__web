dd2a2037b678a7463fccf863b2fda4ae97599fdc
idacarlin95 <carlin95@hotmail.se>
2025-06-16 14:26:28 +0200

    Made the hero section into a component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,9 @@
-import Introduction from "@/components/Introduction";
+import Hero from "@/components/Hero";
 
 const Home = () => {
   return (
-    <div id="main_page" className="relative w-full h-screen">
-      <video
-        autoPlay
-        loop
-        muted
-        playsInline
-        className="absolute top-0 left-0 w-full h-full object-cover z-0"
-      >
-        <source src="/video-playback.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-      <div className="relative z-10 flex flex-col items-center justify-center min-h-screen px-4 sm:px-20 bg-black/40">
-        <main className="flex flex-col gap-8 items-center text-center">
-          <Introduction />
-        </main>
-      </div>
+    <div>
+      <Hero />
     </div>
   );
 }
--- src/components/Introduction/index.tsx
+++ src/components/Introduction/index.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Introduction = () => {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen -mt-48 text-center px-4">
+        <div className="flex flex-col items-center justify-center min-h-screen -mt-12 text-center px-4">
             <Image className="drop-shadow-[-10px_8px_rgba(0,0,0,0.50)]"
                 src="/pirate_placeholder_Logo.png"
                 alt="Absolute Shipshow Logo"
@@ -10,7 +10,7 @@ const Introduction = () => {
                 height={300}
                 priority
             />
-            <p className="text-black font-asheader text-lg sm:text-2xl leading-relaxed max-w-md mt-6">
+            <p className="text-white font-asheader text-lg sm:text-2xl leading-relaxed max-w-md mt-6">
                 Chaos reigns on the seven seas - set sail, grab whatever you can, and battle your way through the mayhem!
             </p>
             <a className="mt-6 bg-white hover:bg-[#9f9f9f] text-red-600 text-base px-6 py-3 rounded-3xl cursor-pointer transition-all"
