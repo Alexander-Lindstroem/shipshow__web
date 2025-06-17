0a09af763477486a327bdbf90b8ce92c7241911e
kavitha0691 <kavitha0691@gmail.com>
2025-06-18 00:55:26 +0200

    implement selector button and prevent content enter into sidebar

--- src/components/CharacterSelector/index.tsx
+++ src/components/CharacterSelector/index.tsx
@@ -18,13 +18,13 @@ export default function CharacterSelector({
           <button
             key={char.id}
             onClick={() => setSelectedId(char.id)}
-            className={`relative  h-24 sm:w-32 sm:h-32 md:w-44 md:h-62 lg:w-58 lg:h-64 focus:outline-none mb-16  ml-0 lg:ml-0 md:ml-0 sm:ml-0 ${index !== characters.length - 1 ? "sm:mr-6" : ""
+            className={`relative h-20 sm:w-20 sm:h-32 md:w-48 md:h-58 lg:w-50 lg:h-48 focus:outline-none mb-16 mt-0 lg:mt-5 ml-0 lg:ml-2 md:ml-0 sm:ml-0 ${index !== characters.length - 1 ? "sm:mr-6" : ""
               }`}
             aria-pressed={isActive}
           >
             {isActive ? (
               <div
-                className="relative w-38 h-36 sm:w-34 sm:h-34 md:w-[190px] md:h-[190px] mt-8 sm:mt-3 md:mt-0 lg:mt-0 mr-10 sm:mr-2 md:mr-0 lg:mr-0 ml-0 sm:ml-1 md:ml-0 lg:ml-0 overflow-visible"
+                className="relative w-27 h-28 md:w-[160px] md:h-[160px] lg:w-[180px] lg:h-[180px] mt-8 sm:mt-3 md:mt-0 lg:mt-0 mr-6 sm:mr-2 md:mr-0 lg:mr-0 ml-0 sm:ml-1 md:ml-0 lg:ml-0 overflow-visible"
               >
                 <Image
                   src={char.posterImage}
@@ -35,14 +35,14 @@ export default function CharacterSelector({
                 />
                 <div
                   className=" absolute
-                           w-30 h-30
-                           top-8 left-1/2 -translate-x-1/2
+                           w-22 h-22.5
+                           top-5 left-1/2 -translate-x-1/2
                            sm:top-5 sm:left-5            
                            sm:w-32 sm:h-32
-                           md:top-10 md:left-6 md:translate-x-0
-                           md:w-[140px] md:h-[145px]
+                           md:top-8 md:left-5 md:translate-x-0
+                           md:w-[123px] md:h-[126px]
                            lg:top-10 lg:left-6 lg:translate-x-0
-                           lg:w-[142px] lg:h-[148px]
+                           lg:w-[135px] lg:h-[138px]
                           flex items-center z-10 p-2 overflow-hidden"
                 >
                   <Image
@@ -55,22 +55,21 @@ export default function CharacterSelector({
                 </div>
                 <div
                   className="
-                          absolute bottom-0 left-1/2 -translate-x-1/2 top-35 text-bold
-                          lg:top-45  sm:top-45 md:top-45
+                          absolute bottom-0 left-1/2 -translate-x-1/2 top-24 text-bold
+                          lg:top-42  sm:top-45 md:top-37
                           lg:bottom-0  sm:bottom-0 md:bottom-0
                          text-yellow-900 text-center font-bold
                           z-20 w-full py-1
                           sm:text-xs text-sm"
                 >
-                  <p className="font-extrabold text-base">$5000</p>
-                  <p className="text-[10px]">Dead or Alive</p>
+                  <p className="font-extrabold text-xs md:text-base lg:text-base">$5000</p>
+                  <p className="text-[8px] md:text-[10px] lg:text-[10px]">Dead or Alive</p>
                 </div>
               </div>
             ) : (
-              <div className=" relative w-30 h-32  mt-12 ml-0 mr-12
-                     lg:w-[10rem] lg:h-[11rem] lg:ml-1 lg:mt-6 lg:mr-0
-                     sm:w-[5rem] sm:h-[5rem] sm:ml-0 sm:mr-5
-                     md:w-[8rem] md:h-[10rem] md:ml-2 md:mt-5
+              <div className=" relative w-22 h-26  mt-12 ml-0 mr-6
+                     lg:w-[9rem] lg:h-[11rem] lg:ml-4 lg:mt-6 lg:mr-0
+                     md:w-[8rem] md:h-[9rem] md:ml-4 md:mt-5
                      rounded-lg  border-2 border-solid border-inherit    
                      overflow-hidden">
                 <Image
