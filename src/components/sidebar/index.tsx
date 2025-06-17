e91f77d58b8627d8898344268a78bbc2f68a0b4c
VaishaliGovindaraj <vaishali.govindaraj28@gmail.com>
2025-06-17 16:21:08 +0200

    adding sidebar changes

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -29,7 +29,8 @@ const Sidebar = ({ sectionChosed, setSection }: sidebarProps) => {
             <div className="fixed top-1/2 -translate-y-1/2 z-50 ml-2"
             >
 
-                <div className="relative w-[150px] h-[350px] pointer-events-none">
+                <div className="relative w-[150px] h-[350px]"  onMouseEnter={() => { setHovering(true) }}
+                                onMouseLeave={() => { setHovering(false) }}>
                     <Image
                         src="/pirate_side_nav_planks.png"
                         alt="plank"
@@ -39,8 +40,7 @@ const Sidebar = ({ sectionChosed, setSection }: sidebarProps) => {
 
                     <div className="absolute inset-0  z-10 flex flex-col justify-center gap-4 pointer-events-auto">
                         {chest_names.map((item, index) => (
-                            <div className="relative group min-[769px]:ml-8 max-w-max" key={index} onMouseEnter={() => { setHovering(true) }}
-                                onMouseLeave={() => { setHovering(false) }}>
+                            <div className="relative group min-[769px]:ml-8 max-w-max" key={index}>
                                 <Image
                                     src={item === sectionChosed ? `/pirate_Openchest.png` : `/pirate_Closedchest.png`}
                                     alt={`chest ${index + 1}`}
@@ -57,7 +57,7 @@ const Sidebar = ({ sectionChosed, setSection }: sidebarProps) => {
                                 </div>
 
 
-                                <div className="max-[769px]:block hidden ml-2 text-white bg-gray-700 px-2 text-[1.25rem] font-asheader whitespace-nowrap text-center" onClick={() => setHovering(false)}>
+                                <div className="max-[769px]:block hidden  text-white bg-gray-700 px-2 text-[1.25rem] font-asheader whitespace-nowrap text-center" onClick={() => setHovering(false)}>
                                     {item === "main_page" ? "A Hoy!" : item.replace("_", " ")}
                                 </div>
                             </div>
