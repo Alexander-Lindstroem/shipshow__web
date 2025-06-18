0113100b51e525f8135735f3d2c4d1c6074dd684
kavitha0691 <kavitha0691@gmail.com>
2025-06-18 14:57:09 +0200

    fix misaligned images on 768px

--- src/components/CharacterSelector/index.tsx
+++ src/components/CharacterSelector/index.tsx
@@ -9,7 +9,7 @@ export default function CharacterSelector({
   setSelectedId,
 }: CharacterSelectorProps) {
   return (
-    <div className="flex justify-center items-center max-w-[80%] sm:max-w-[80%] md:max-w-[60%] lg:max-w-[60%] mx-auto ">
+    <div className="flex max-[350px]:flex-wrap justify-center items-center max-w-[80%] sm:max-w-[80%] md:max-w-[60%] lg:max-w-[60%] mx-auto gap-4">
 
       {characters.map((char, index) => {
         const isActive = char.id === selectedId;
@@ -18,13 +18,13 @@ export default function CharacterSelector({
           <button
             key={char.id}
             onClick={() => setSelectedId(char.id)}
-            className={`relative h-20 sm:w-20 sm:h-32 md:w-48 md:h-58 lg:w-50 lg:h-48 focus:outline-none mb-16 mt-0 lg:mt-5 ml-0 lg:ml-2 md:ml-0 sm:ml-0 ${index !== characters.length - 1 ? "sm:mr-6" : ""
+            className={`relative h-20 md:w-48 md:h-58 lg:w-50 lg:h-48 focus:outline-none mb-16 mt-0 lg:mt-5 ml-0 lg:ml-2 md:ml-0  ${index !== characters.length - 1 ? "sm:mr-6" : ""
               }`}
             aria-pressed={isActive}
           >
             {isActive ? (
               <div
-                className="relative w-27 h-28 md:w-[160px] md:h-[160px] lg:w-[180px] lg:h-[180px] mt-8 sm:mt-3 md:mt-0 lg:mt-0 mr-6 sm:mr-2 md:mr-0 lg:mr-0 ml-0 sm:ml-1 md:ml-0 lg:ml-0 overflow-visible "
+                className="relative w-27 h-28 md:w-[144px] md:h-[144px] lg:w-[160px] lg:h-[160px] mt-8  md:mt-0 lg:mt-0 mr-6  md:mr-0 lg:mr-0 ml-0  md:ml-0 lg:ml-0 overflow-visible "
               >
                 <Image
                   src={char.posterImage}
@@ -36,13 +36,11 @@ export default function CharacterSelector({
                 <div
                   className=" absolute
                            w-22 h-22.5
-                           top-5 left-1/2 -translate-x-1/2
-                           sm:top-5 sm:left-5            
-                           sm:w-32 sm:h-32
-                           md:top-8 md:left-5 md:translate-x-0
-                           md:w-[123px] md:h-[126px]
-                           lg:top-10 lg:left-6 lg:translate-x-0
-                           lg:w-[135px] lg:h-[138px]
+                           top-5 left-1/2 -translate-x-1/2            
+                           md:top-7 md:left-4 md:translate-x-0
+                           md:w-[110px] md:h-[115px]
+                           lg:top-8 lg:left-5 lg:translate-x-0
+                           lg:w-[122px] lg:h-[126px]
                           flex items-center z-10 p-2 overflow-hidden
                           "
                 >
@@ -57,11 +55,11 @@ export default function CharacterSelector({
                 <div
                   className="
                           absolute bottom-0 left-1/2 -translate-x-1/2 top-24 text-bold
-                          lg:top-42  sm:top-45 md:top-37
-                          lg:bottom-0  sm:bottom-0 md:bottom-0
+                          lg:top-36   md:top-32
+                          lg:bottom-0  md:bottom-0
                          text-yellow-900 text-center font-bold
                           z-20 w-full py-1
-                          sm:text-xs text-sm"
+                          text-sm"
                 >
                   <p className="font-extrabold text-xs md:text-base lg:text-base">$5000</p>
                   <p className="text-[8px] md:text-[10px] lg:text-[10px]">Dead or Alive</p>
@@ -69,8 +67,8 @@ export default function CharacterSelector({
               </div>
             ) : (
               <div className=" relative w-22 h-26  mt-12 ml-0 mr-6
-                     lg:w-[9rem] lg:h-[11rem] lg:ml-4 lg:mt-6 lg:mr-0
-                     md:w-[8rem] md:h-[9rem] md:ml-4 md:mt-5
+                     lg:w-[8rem] lg:h-[10rem] lg:ml-2 lg:mt-7 lg:mr-0
+                     md:w-[7rem] md:h-[8rem] md:ml-4 md:mt-5
                      rounded-lg  border-2 border-solid border-inherit    
                      overflow-hidden">
                 <Image
