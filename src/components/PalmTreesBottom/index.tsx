819c0d912e85adc9df4db2bea2294a1ca2aed705
VaishaliGovindaraj <vaishali.govindaraj28@gmail.com>
2025-06-18 22:13:59 +0200

    adding changes

--- src/components/PalmTreesBottom/index.tsx
+++ src/components/PalmTreesBottom/index.tsx
@@ -6,7 +6,7 @@ const PalmTreesBottom = () => {
       <motion.img
         src="/pirate_palmtree-bottom.png"
         alt="Palm Tree Bottom"
-        className="absolute right-0 top-0 translate-y-6 w-32 sm:w-48 drop-shadow-[-10px_8px_rgba(0,0,0,0.50)]"
+        className="absolute right-0 bottom-0 translate-y-6 w-32 sm:w-48 drop-shadow-[-10px_8px_rgba(0,0,0,0.50)]"
         style={{ transformOrigin: 'bottom center' }}
         animate={{ rotate: [0, 2, 0, -2, 0] }}
         transition={{ duration: 5, repeat: Infinity, ease: 'easeInOut' }}
