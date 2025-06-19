41701ee25664be70faa1f205f5104717cae63b9a
alelin <alexanderbdjgw@gmail.com>
2025-06-19 17:25:48 +0200

    changed header to absolute

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,24 @@
 import Navigation from "../Navigation"
 import Logo from "../Logo"
+import Image from "next/image"
 
 const Header = () => {
     return (
-        <div className="w-screen border-black-100 border bg-black opacity-70 shadow-xl md:w z-50 fixed">
-            <header className=" mx-4 my-4 flex justify-between items-center h-auto gap-4 md:mx-20 md:flex-row  md:h-24">
+        <div className="w-screen bg-no-repeat z-50 top-0 absolute">
+            <div className="absolute w-[120%] h-full z-60 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                    <Image
+                        src="/pirate-plank.png"
+                        alt="Footer Plank"
+                        fill
+                        priority
+                        className="object-fill shadow-orange-950 shadow-md"
+                    />
+            </div>
+            <header className="relative z-70 mx-4 my-4 flex justify-between items-center h-auto gap-4 md:mx-20 md:flex-row md:h-24">
                 <Logo/>
                 <Navigation/>
             </header>
+            
         </div>
     )
 }
