7fa6e5b2010a7b21c9d36898009a26fd4fcd3ab1
VaishaliGovindaraj <vaishali.govindaraj28@gmail.com>
2025-06-23 20:15:45 +0200

    fixing shadow

--- src/components/PalmTreesLeftRight/index.tsx
+++ src/components/PalmTreesLeftRight/index.tsx
@@ -10,7 +10,7 @@ const PalmTreesLeftRight = () => {
   alt="Palm Tree Left"
   className="absolute left-0 top-[-300]  transform rotate-70 
             hidden  md:block md:w-[300px]  md:top-10 md:left-[-230] md:scale-125 lg:left-[-530] lg:block lg:w-[650px]
-             drop-shadow-[-10px_8px_rgba(0,0,0,0.50)]"
+             drop-shadow-[-10px_8px_rgba(0,0,0,0.30)]"
   animate={{ rotate: [0, -3, 0, 3, 0] }}
   transition={{ duration: 4, repeat: Infinity, ease: 'easeInOut' }}
 />
@@ -20,7 +20,7 @@ const PalmTreesLeftRight = () => {
   alt="Palm Tree Right"
   className="absolute right-0 top-[-150] -translate-y-[120px] transform rotate-360
              hidden md:block md:w-[400px] md:right-[-300] md:top-10 md:scale-120 lg:right-[-530px] lg:block lg:w-[700px] 
-             drop-shadow-[-10px_8px_rgba(0,0,0,0.50)]"
+             drop-shadow-[-10px_8px_rgba(0,0,0,0.30)]"
   animate={{ rotate: [0, 1, 0, -1, 0] }}
   transition={{ duration: 4, repeat: Infinity, ease: 'easeInOut' }}
 />
