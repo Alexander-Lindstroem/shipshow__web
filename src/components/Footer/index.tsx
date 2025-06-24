2ba29512c727c9d9039c250a2ed1b31edc4f6c46
VaishaliGovindaraj <vaishali.govindaraj28@gmail.com>
2025-06-24 14:07:26 +0200

    removing navigation icons from footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,34 +4,35 @@ import Logo from '../Logo';
 
 const Footer = () => {
   return (
-    <footer className="relative w-full h-[120px] overflow-hidden bg-black ">
-      <div className="absolute inset-0 z-0">
-        <Image
-          src="/pirate-plank.png"
-          alt="Footer Plank"
-          fill
-          priority
-          className="object-cover"
-        />
-      </div>
+   <footer className="relative w-screen h-[120px] overflow-hidden bg-[#623D17]">
+  <div className="absolute inset-0 z-0">
+    <Image
+      src="/pirate-plank.png"
+      alt="Footer Plank"
+      fill
+      priority
+      className="object-cover"
+    />
+  </div>
 
-      <div className="relative z-10 flex items-center  justify-center w-full h-full px-4 md:px-8">
-        <div className="flex flex-col items-center justify-center text-center text-white font-asheader w-full md:w-auto">
-          <p className="text-3xl md:text-4xl">Absolute ShipShow</p>
-          <p className="text-2xl md:text-2xl">A chaotic pirate co-op adventure</p>
-          <p className="text-xl md:text-xl">
-            Play now:{" "}
-            <a
-              href="https://futuregames.itch.io/absoluteshipshow"
-              target="_blank"
-              className="underline text-blue-300"
-            >
-              futuregames.itch.io/absoluteshipshow
-            </a>
-          </p>
-        </div>
-      </div>
-    </footer>
+  <div className="relative z-10 flex items-center justify-center w-screen h-full">
+    <div className="flex flex-col items-center justify-center text-center text-white font-asheader w-full md:w-auto">
+      <p className="text-3xl md:text-4xl">Absolute ShipShow</p>
+      <p className="text-2xl md:text-2xl">A chaotic pirate co-op adventure</p>
+      <p className="text-xl md:text-xl">
+        Play now:{" "}
+        <a
+          href="https://futuregames.itch.io/absoluteshipshow"
+          target="_blank"
+          className="underline text-blue-300"
+        >
+          futuregames.itch.io/absoluteshipshow
+        </a>
+      </p>
+    </div>
+  </div>
+</footer>
+
   );
 };
 
